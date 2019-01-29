edba6a8bde0602bd4fb5b30660f34b729a9ec93f
breeze-yang <breeze@deshpro.com>
2019-01-29 11:30:34 +0800

    前台功能：完成main event

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module CGAsia
     # sanitize 自定义白名单
     config.action_view.sanitized_allowed_tags = Set.new(%w(strong em b i p code pre tt samp kbd var sub
         sup dfn cite big small address hr br div span h1 h2 h3 h4 h5 h6 ul ol li dl dt dd abbr
-        acronym a img blockquote del ins table))
+        acronym a img blockquote del ins table thead tbody tr th td))
   end
 end
