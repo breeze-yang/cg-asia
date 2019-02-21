245812b26a39bc5133d4062d986fe38dad72783d
breeze-yang <breeze@deshpro.com>
2019-02-21 17:25:03 +0800

    完善news web端的页面

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,7 @@
 class NewsController < ApplicationController
   def index
     @news = Info.where(published: true).id_desc.page(params[:page]).per(10)
+    @news_arr = @news.to_a
   end
 
   def show
