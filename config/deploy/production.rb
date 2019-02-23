f50c4cf0ea11af42cb207ec2602e3e7d36a4dfc7
breeze-yang <breeze@deshpro.com>
2019-02-23 18:26:33 +0800

    跳转中英文链接

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,11 @@
-server '129.211.39.113',
-       user: 'deploy',
-       roles: %w{app web db cache},
-       ssh_options: {
-         user: 'deploy', # overrides user setting above
-         keys: %w(~/.ssh/id_rsa),
-         forward_agent: false,
-         auth_methods: %w(publickey password)
-         # password: 'please use keys'
-       }
-
+server '129.211.39.113', user: 'deploy', roles: %w{app web db cache}
+set :ssh_options, {
+  user: 'deploy', # overrides user setting above
+  keys: %w(~/.ssh/id_rsa),
+  forward_agent: false,
+  auth_methods: %w(publickey password)
+  # password: 'please use keys'
+}
 
 set :branch, ENV.fetch('REVISION', ENV.fetch('BRANCH', 'master'))
 set :rails_env, 'production'
